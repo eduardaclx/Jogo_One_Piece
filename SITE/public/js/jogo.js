ddf8630c6b84079ba7b9b6d42075aa5f68302eaf
eduardaclx <eduarda.evangelista@sptech.school>
2022-06-01 11:37:55 -0300

    finalização da lógica dead

--- SITE/public/js/jogo.js
+++ SITE/public/js/jogo.js
@@ -1,23 +1,38 @@
 var jump = false
+var obstaculo = document.getElementById("obstaculo")
+var qntJump = 0
 
-function jump1(){
-    var player = document.getElementById("player")
-    var obstaculo = document.getElementById("obstaculo")
-
-    if(player.className != "jump"){
-        player.classList.add("jump")
-        setTimeout(()=>{
-            player.classList.remove("jump")
-            jump=false
-        }, 800)
-
-        function dead() {
-            var obstaculo1 = parseInt(window.getComputedStyle(obstaculo).getPropertyValue("left"))
-
-            if(obstaculo1 < 20 && jump == false){
-                alert("game over")
-            }
-        }
-        // setInterval(dead, 80)
+function contadorJump() {
+    if(qntJump > 2){
+        
+        obstaculo.style.right = "200vw"
+        obstaculo.style.animation = "obstaculo 2s infinite linear"
+        obstaculoVoador.style.animation = "obstaculoVoador 10s infinite linear"
+        console.log("aaaaaaaaaaaaaa")
     }
 }
+
+function jump1() {
+    var player = document.getElementById("player")
+    if (player.className != "jump") {
+        player.innerHTML = `<img src="../pngFinalizadas/jump.gif"/>`
+        jump = true
+        player.classList.add("jump")
+        setTimeout(() => {
+            player.classList.remove("jump")
+            jump = false
+            player.innerHTML = `<img src="../pngFinalizadas/run.gif"/>`
+            qntJump ++
+        }, 1000)
+ 
+    }
+}
+function dead() {
+    var obstaculo1 = parseInt(window.getComputedStyle(obstaculo).getPropertyValue("left"))
+    console.log("aaa");
+    if (obstaculo1 < 190 && jump == false) {
+        alert("game over")
+    }
+}
+setInterval(dead, 30)
+setInterval(contadorJump, 20)
